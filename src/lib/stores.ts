8d72d84235d6cb54aca64aec98bfcfdf1d814b30
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-12-19 16:21:57 -0500

    reimplement transforms at matrix

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,4 @@
 import {writable} from 'svelte/store';
 
 export const size = writable([0, 0]);
-export const position = writable([0, 0]);
-export const zoom = writable(1);
-export const origin = writable([0, 0]);
+export const matrix = writable([1, 0, 0, 1, 0, 0]);
