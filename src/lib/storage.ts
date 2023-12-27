b14dd74a7eb59cf26198c54530e78651a5cd53d1
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-12-27 13:22:09 -0500

    move storage to related util files

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,25 +1,16 @@
-import { frames as framesStore, matrix as matrixStore, size as sizeStore } from "$lib/stores";
+import { size as sizeStore } from "$lib/stores";
 import { get } from "svelte/store";
 
-// auto-save on change
-framesStore.subscribe((frames) => {
-  localStorage.setItem("frames", JSON.stringify(frames));
-});
-
-matrixStore.subscribe((matrix) => {
-  localStorage.setItem("matrix", JSON.stringify(matrix));
-});
-
-sizeStore.subscribe((size) => {
-  localStorage.setItem("size", JSON.stringify(size));
-});
+export const setupAutoSave = () => {
+  sizeStore.subscribe((size) => {
+    localStorage.setItem("size", JSON.stringify(size));
+  });
+}
 
 /**
  * Manually save all stores to localStorage
  */
 export const save = () => {
-  localStorage.setItem("frames", JSON.stringify(get(framesStore)));
-  localStorage.setItem("matrix", JSON.stringify(get(matrixStore)));
   localStorage.setItem("size", JSON.stringify(get(sizeStore)));
 };
 
@@ -27,17 +18,7 @@ export const save = () => {
  * Manually load and update all stores from localStorage, if available
  */
 export const load = () => {
-  const frames = localStorage.getItem("frames");
-  const matrix = localStorage.getItem("matrix");
   const size = localStorage.getItem("size");
 
-  // we only want to update the store if there is something to update it with
-  if (frames !== null)
-    framesStore.set(JSON.parse(frames));
-
-  if (matrix !== null)
-    matrixStore.set(JSON.parse(matrix));
-
-  if (size !== null)
-    sizeStore.set(JSON.parse(size));
+  if (size !== null) sizeStore.set(JSON.parse(size));
 };
