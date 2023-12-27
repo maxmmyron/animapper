1e9483a564ce41317a430be39f6a2613e5d614ea
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-12-27 13:21:49 -0500

    load frames from storage

--- src/lib/frames.ts
+++ src/lib/frames.ts
@@ -1,26 +1,53 @@
+import { get } from "svelte/store";
+import { frames, bg } from "./stores";
+
 /**
  * Creates an empty frame object
  *
  * @param canvas Canvas element to generate empty frame image from
  * @returns {App.Frame} Empty frame object
  */
-export const createEmptyFrame = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, fill: string): App.Frame => {
+export const createEmptyFrame = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D): App.Frame => {
   // capture overlay source as transparent image
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   const overlaySrc = canvas.toDataURL();
 
   // capture initial render source as filled image
-  ctx.fillStyle = fill;
+  ctx.fillStyle = get(bg);
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   return {
     dirty: false,
-    background: fill,
+    background: get(bg),
     renderSrc: canvas.toDataURL(),
     overlaySrc,
     redoStack: [],
     undoStack: [],
   };
+};
+
+/**
+ * Loads frames on page load by checking localStorage for stored frames, or
+ * creating a new frame if none are found.
+ * @param canvas
+ * @param ctx
+ * @param fill
+ */
+export const loadFrames = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D): App.Frame[] => {
+  const storedFrames = localStorage.getItem("frames");
+  if (storedFrames !== null) {
+    console.log("Found stored frames, loading...");
+    const storedFramesObj = JSON.parse(storedFrames);
+    console.log(storedFramesObj);
+    return storedFramesObj;
+  } else {
+    console.log("No stored frames found, creating new frame");
+    return [createEmptyFrame(canvas, ctx)];
+  }
+};
+
+export const saveFramesToStorage = () => {
+  localStorage.setItem("frames", JSON.stringify(frames));
 };
 
 
