7c18ccfd1d8767be3b96153c011fd9f2bdceea8c
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-12-29 13:38:40 -0500

    use explicit tuple writable type

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,7 +2,7 @@ import { createFFmpeg, type FFmpeg } from '@ffmpeg/ffmpeg';
 import {writable, type Writable} from 'svelte/store';
 
 export const frameIdx = writable(0);
-export const size = writable([0, 0]);
+export const size: Writable<[number,number]> = writable([0, 0]);
 export const bg = writable(`#ffffff`);
 export const matrix = writable([0.9, 0, 0, 0.9, 0, 0]);
 export const frames: Writable<App.Frame[]> = writable([]);
