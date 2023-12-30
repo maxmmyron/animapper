b896d9c19f55c162fd7da57b14b23f96223e57e5
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-12-30 13:30:35 -0500

    implement derived exportable size

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,8 +1,19 @@
 import { createFFmpeg, type FFmpeg } from '@ffmpeg/ffmpeg';
-import {writable, type Writable} from 'svelte/store';
+import {derived, writable, type Writable} from 'svelte/store';
 
 export const frameIdx = writable(0);
 export const size: Writable<[number,number]> = writable([0, 0]);
+
+/**
+ * A rounded size that is safe to send to ffmpeg for export (because ffmpeg
+ * requires dimensions to be divisible by 2)
+ */
+export const exportSafeSize = derived(size, ([width, height]) => {
+  // round to nearest multiple of 2
+  // i.e. 3 -> 2, 4 -> 4, 5 -> 4, 6 -> 6, etc.
+  return [Math.floor((width) / 2) * 2, Math.floor((height) / 2) * 2];
+});
+
 export const bg = writable(`#ffffff`);
 export const matrix = writable([0.9, 0, 0, 0.9, 0, 0]);
 export const frames: Writable<App.Frame[]> = writable([]);
