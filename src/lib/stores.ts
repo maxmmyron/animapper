b5b1b71b06e15640df1274b400149470e07494ba
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-12-30 13:46:33 -0500

    force tuple type on derived store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,5 @@
 import { createFFmpeg, type FFmpeg } from '@ffmpeg/ffmpeg';
-import {derived, writable, type Writable} from 'svelte/store';
+import {derived, writable, type Readable, type Writable} from 'svelte/store';
 
 export const frameIdx = writable(0);
 export const size: Writable<[number,number]> = writable([0, 0]);
@@ -8,10 +8,10 @@ export const size: Writable<[number,number]> = writable([0, 0]);
  * A rounded size that is safe to send to ffmpeg for export (because ffmpeg
  * requires dimensions to be divisible by 2)
  */
-export const exportSafeSize = derived(size, ([width, height]) => {
+export const exportSafeSize: Readable<[number,number]> = derived(size, ([width, height]) => {
   // round to nearest multiple of 2
   // i.e. 3 -> 2, 4 -> 4, 5 -> 4, 6 -> 6, etc.
-  return [Math.floor((width) / 2) * 2, Math.floor((height) / 2) * 2];
+  return [Math.floor((width) / 2) * 2, Math.floor((height) / 2) * 2] as [number, number];
 });
 
 export const bg = writable(`#ffffff`);
