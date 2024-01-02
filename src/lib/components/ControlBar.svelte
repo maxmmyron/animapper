1ed65fe129b51054042f4f69110c673e3fcf8d5a
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-01-01 20:41:56 -0500

    improve looks of color picker

--- src/lib/components/ControlBar.svelte
+++ src/lib/components/ControlBar.svelte
@@ -4,15 +4,22 @@
 
 <section class="flex justify-center items-center gap-4">
   <button
-    class="px-3 py-1 rounded-md hover:bg-gray-100 transition-colors"
+    class="px-3 py-1 rounded-md hover:bg-gray-100 transition-colors cursor-pointer"
     on:click={() => ($isPlaying = !$isPlaying)}
     disabled={$frames.length == 0}
   >
-    {$isPlaying ? "Pause" : "Play"}
+    <p class="text-sm">{$isPlaying ? "Pause" : "Play"}</p>
   </button>
 
-  <label class="flex items-center gap-2">
-    bg
-    <input type="color" bind:value={$bg} />
+  <label
+    class="px-3 py-1 rounded-md flex flex-col items-center hover:bg-gray-100 transition-colors cursor-pointer"
+  >
+    <p class="text-sm cursor-pointer">Frame BG</p>
+    <input
+      class="h-4 w-4/5 cursor-pointer"
+      type="color"
+      id="color"
+      bind:value={$bg}
+    />
   </label>
 </section>
